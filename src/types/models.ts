bc1561945fe751e3e2fa8bf7b9a5225ccabdd148
JChuck0 <110050395+JChuck0@users.noreply.github.com>
2026-08-16 14:58:44 +0000

    Terminamos de definir models.ts

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,33 +1,35 @@
 export interface Envio {
     idEnvio:string,
-    idPedido:string,
+    idProducto:string,
     idTransportista:string,
     pais:('US' | 'ES'),
-    estado :('pendiente' | 'en_transito' | 'entregado' | 'incidencia')
-    pesoKg:number,
+    estado :('pendiente' | 'en_transito' | 'entregado' | 'incidencia'),
     costeEnvio:number,
+    pesoTotal:number,
     fechaSalida:Date,
     fechaLlegada:Date
 }
 export interface Producto {
     idProducto:string,
     idEmpresa:string,
+    pesoKg:number,
     descripcion:string,
-    fechaEntrada: Date
+    precio:number,
+    cantidad:number
 }
 export interface Transportista {
     idTransportista: string,
-    nombre:string,
-    pais:('US' | 'ES')
+    nombreEmpresa:string,
+    pais:('US' | 'ES'),
     tasaEntregaATiempo: number,
     costePorKg: number,
-    incideciasRegistradas: string
+    incideciasRegistradas: string[]
 }
 export interface Devolución {
     idDevolucion:string,
-    idPedido:string,
+    idEnvio:string,
+    idProducto:string,
     motivo:string,
     estado: ('pendiente' | 'aprobada' | 'rechazada'),
-    fechaSolicitud:Date,
-    pais:('US' | 'ES')
+    fechaSolicitud:Date
 }
